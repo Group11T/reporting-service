28b3de073ac8aa3519581a1bd86144ac67738624
kbbansi <kwabenaampofo5@gmail.com>
2021-03-24 12:25:19 +0000

    initial commit

---
+++ src/test/java/com/io/t11/reportingservice/ReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.t11.reportingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
